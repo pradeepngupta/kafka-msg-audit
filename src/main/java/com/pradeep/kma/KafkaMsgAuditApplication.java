10b7e14937b95c490fba81708c90f5132f58ff09
Pradeep Gupta <pradeepngupta@gmail.com>
2025-06-22 18:32:42 +0530

    Initial project creation using Spring initializr

---
+++ src/main/java/com/pradeep/kma/KafkaMsgAuditApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep.kma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMsgAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMsgAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradeep/kma/KafkaMsgAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.kma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMsgAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
